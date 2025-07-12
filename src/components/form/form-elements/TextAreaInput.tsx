1943ea26efd4c867a1bfb4f6c31e180d478e0e61
ramkumar.r <ramkumar.r@abserve.tech>
2025-07-12 10:45:12 +0530

    Build issue comment

--- src/components/form/form-elements/TextAreaInput.tsx
+++ src/components/form/form-elements/TextAreaInput.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import ComponentCard from "../../common/ComponentCard";
 import TextArea from "../input/TextArea";
 import Label from "../Label";
 
 export default function TextAreaInput() {
-  const [message, setMessage] = useState("");
-  const [messageTwo, setMessageTwo] = useState("");
+  // const [message, setMessage] = useState("");
+  // const [messageTwo, setMessageTwo] = useState("");
   return (
     <ComponentCard title="Textarea input field">
       <div className="space-y-6">
@@ -13,8 +13,8 @@ export default function TextAreaInput() {
         <div>
           <Label>Description</Label>
           <TextArea
-            value={message}
-            onChange={(value) => setMessage(value)}
+            // value={message}
+            // onChange={(value) => setMessage(value)}
             rows={6}
           />
         </div>
@@ -30,9 +30,9 @@ export default function TextAreaInput() {
           <Label>Description</Label>
           <TextArea
             rows={6}
-            value={messageTwo}
+            // value={messageTwo}
             error
-            onChange={(value) => setMessageTwo(value)}
+            // onChange={(value) => setMessageTwo(value)}
             hint="Please enter a valid message."
           />
         </div>
